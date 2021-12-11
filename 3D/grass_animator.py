2292640a241236c0f063c929b963defd9362122f
Michael Moyal <moyalmic@fit.cvut.cz>
2021-12-11 11:30:53 +0100

    Fixed bugs and added more functionality to the grass field generator

--- 3D/grass_animator.py
+++ 3D/grass_animator.py
@@ -15,18 +15,13 @@ from math import *
 from bpy.props import *
 
 
-turbulence = None
-wind = None
-grass_plane = None
-
-
 def wind_update_func(self, context):
     wind_properties = context.scene.wind_properties
-    wind.field.strength = wind_properties.wind_strength 
-    turbulence.field.size = (-10 * wind_properties.wind_strength) + 10
+    context.scene.wind_object.field.strength = wind_properties.wind_strength 
+    context.scene.turbulence_object.field.size = (-10 * wind_properties.wind_strength) + 10
     
 def angle_update_func(self, context):
-    wind.rotation_euler[2] = context.scene.wind_properties.angle
+    context.scene.wind_object.rotation_euler[2] = context.scene.wind_properties.angle
 
 class AnimationProperty(bpy.types.PropertyGroup):
     
@@ -85,19 +80,18 @@ class GrassAnimator(bpy.types.Operator):
         wind_properties = context.scene.wind_properties
         bpy.ops.object.effector_add(type='WIND',radius=wind_properties.wind_strength, align='CURSOR', 
         rotation=(-90.0, 0.0, wind_properties.angle))
-        wind = bpy.data.objects['Wind']
+        context.scene.wind_object = bpy.context.active_object
         turbulence_location = (active_obj.location.x, active_obj.location.y-active_obj.scale.y, active_obj.location.z)
         bpy.ops.object.effector_add(type='TURBULENCE', align='WORLD', location=turbulence_location)
-        turbulence = bpy.data.objects['Turbulence']
-        turbulence.field.size = (-10 * wind_properties.wind_strength) + 10
+        context.scene.turbulence_object = bpy.context.active_object
+        context.scene.turbulence_object.field.size = (-10 * wind_properties.wind_strength) + 10
     
     def animate_keyframes(self, context):
-        turbulence.select_set(True)
+        context.scene.turbulence_object.select_set(True)
         bpy.context.object.keyframe_insert(data_path='location', frame=0)
         plane = self.grass_plane
         context.active_object.location.y += plane.scale.y * 2
         bpy.context.object.keyframe_insert(data_path='location', frame=bpy.data.scenes["Scene"].frame_end)
-
         
         self.set_keyframe_interpolation(context, 'LINEAR')
            
@@ -106,17 +100,42 @@ class GrassAnimator(bpy.types.Operator):
         bpy.ops.action.interpolation_type(type=itp_type)
         bpy.context.area.type = 'VIEW_3D'
         
-# ------------------------------------------------------------------------------------------------------        
+# ------------------------------------------------------------------------------------------------------   
+
+def size_update_func(self, context):
+    object_properties = context.scene.object_properties
+    context.scene.grass_plane_object.scale = object_properties.plane_size
+    
+def length_update_func(self, context):
+    object_properties = context.scene.object_properties
+    context.scene.grass_plane_object.hair_length = object_properties.grass_length
+    
+def base_width_update_func(self, context):
+    object_properties = context.scene.object_properties
+    context.scene.grass_plane_object.root_radius = object_properties.grass_base_width   
+    
+def shape_update_func(self, context):
+    object_properties = context.scene.object_properties
+    context.scene.grass_plane_object.brownian_factor = object_properties.grass_shape  
+    
+def clump_update_func(self, context):
+    object_properties = context.scene.object_properties
+    context.scene.grass_plane_object.clump_factor = object_properties.clumpyness
+     
+def density_update_func(self, context):
+    object_properties = context.scene.object_properties
+    particle_settings = context.scene.grass_plane_object.particle_systems[0].settings.name
+    bpy.data.particles[particle_settings].count = object_properties.grass_density
         
 class ObjectProperty(bpy.types.PropertyGroup):
     
     plane_size : bpy.props.FloatVectorProperty(
     name = 'Plane size',
     description = 'Controls the size of the grass plane',
-    default = (1.0, 1.0, 1.0),
+    default = (5.0, 5.0, 1.0),
     min = 1,
-    step = 10
-    # update = 
+    step = 10,
+    update = size_update_func
     )
     
     grass_length : bpy.props.FloatProperty(
@@ -125,8 +144,8 @@ class ObjectProperty(bpy.types.PropertyGroup):
     default = 1.5,
     min = 1,
     max = 5,
-    step = 1
-    # update = 
+    step = 1,
+    update = length_update_func
     )  
     
     grass_base_width : bpy.props.FloatProperty(
@@ -135,8 +154,8 @@ class ObjectProperty(bpy.types.PropertyGroup):
     default = 2,
     min = 1,
     max = 3,
-    step = 1
-    # update =
+    step = 1,
+    update = base_width_update_func
     )
         
     grass_shape : bpy.props.FloatProperty(
@@ -145,8 +164,8 @@ class ObjectProperty(bpy.types.PropertyGroup):
     default = 0,
     min = 0,
     max = 0.3,
-    step = 1
-    # update =
+    step = 1,
+    update = shape_update_func
     )
     
     clumpyness : bpy.props.FloatProperty(
@@ -155,8 +174,17 @@ class ObjectProperty(bpy.types.PropertyGroup):
     default = 0,
     min = -1.0,
     max = 0,
-    step = 1
-    # update =
+    step = 1,
+    update = clump_update_func
+    )
+    
+    grass_density : bpy.props.IntProperty(
+    name = 'Grass Density',
+    description = 'Controls how many blades of grass there are',
+    default = 500,
+    min = 0,
+    step = 100,
+    update = density_update_func
     )
     
         
@@ -183,6 +211,8 @@ class GeneratorPanel(bpy.types.Panel):
         row = layout.row(align=True)
         row.prop(object_properties, "clumpyness")
         row = layout.row(align=True)
+        row.prop(object_properties, "grass_density")
+        row = layout.row(align=True)
         row.operator("object.grass_plane")
 
         
@@ -193,22 +223,32 @@ class GrassGenerator(bpy.types.Operator):
     
     def execute(self, context):
         self.generate_plane(context)
+        return {'FINISHED'}
         
     def generate_plane(self, context):
         object_properties = context.scene.object_properties
         bpy.data.scenes["Scene"].render.hair_type = 'STRIP'
-        bpy.ops.mesh.primitive_plane_add(scale=object_properties.plane_size)
+        bpy.ops.mesh.primitive_plane_add(align='WORLD', scale=object_properties.plane_size)
+        context.scene.grass_plane_object = bpy.context.active_object
         bpy.ops.object.particle_system_add()
-        bpy.data.particles["ParticleSettings"].type = 'HAIR'
-        # TODO Make this next line take the average of all 3 dimensions and multiply by 10
-        bpy.data.particles["ParticleSettings"].count = 500
-        bpy.data.particles["ParticleSettings"].use_advanced_hair = True
-        bpy.data.particles["ParticleSettings"].hair_length = object_properties.grass_length
-        bpy.data.particles["ParticleSettings"].root_radius = object_properties.grass_base_width
-        bpy.data.particles["ParticleSettings"].brownian_factor = object_properties.grass_shape
-        bpy.data.particles["ParticleSettings"].child_type = 'INTERPOLATED'
-        bpy.data.particles["ParticleSettings"].child_nbr = 3
-        bpy.data.particles["ParticleSettings"].clump_factor = object_properties.clumpyness
+        particle_settings = context.scene.grass_plane_object.particle_systems[0].settings.name
+        bpy.data.particles[particle_settings].type = 'HAIR'
+        bpy.data.particles[particle_settings].count = object_properties.grass_density
+        bpy.data.particles[particle_settings].use_advanced_hair = True
+        bpy.data.particles[particle_settings].hair_length = object_properties.grass_length
+        bpy.data.particles[particle_settings].root_radius = object_properties.grass_base_width
+        bpy.data.particles[particle_settings].brownian_factor = object_properties.grass_shape
+        bpy.data.particles[particle_settings].child_type = 'INTERPOLATED'
+        bpy.data.particles[particle_settings].child_nbr = 3
+        bpy.data.particles[particle_settings].clump_factor = object_properties.clumpyness
+        
+        
+    def average_vector(self, vec):
+        acc = 0
+        for scalar in range(0, len(vec)):
+            acc += scalar
+        acc /= len(vec)
+        return int(acc)
         
         
     
@@ -227,8 +267,13 @@ def register():
         register_class(cls)
     bpy.types.VIEW3D_MT_object_animation.append(anim_menu_func)
     bpy.types.VIEW3D_MT_mesh_add.append(grass_menu_func)
+    # Storage for settings needed by the individual generators
     bpy.types.Scene.wind_properties = PointerProperty(type=AnimationProperty)
     bpy.types.Scene.object_properties = PointerProperty(type=ObjectProperty)
+    # Storage for objects created by the plugin and allowing access
+    bpy.types.Scene.wind_object = PointerProperty(type=bpy.types.Object)
+    bpy.types.Scene.turbulence_object = PointerProperty(type=bpy.types.Object)
+    bpy.types.Scene.grass_plane_object = PointerProperty(type=bpy.types.Object)
         
         
 def unregister():
@@ -241,4 +286,3 @@ def unregister():
     
 if __name__ == "__main__":
     register()
-    
